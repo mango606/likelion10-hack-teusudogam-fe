a5d75585cedee6b733dbf5378b6e59a237e4aec1
cstria0106 <cstria0106@gmail.com>
2022-08-17 00:40:38 +0900

    뱃지 생성 구현

--- src/components/badge-production/badge-production.jsx
+++ src/components/badge-production/badge-production.jsx
@@ -1,169 +1,249 @@
-import { css, useTheme } from '@emotion/react';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useCallback, useMemo } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FormProvider, useForm, useFormContext } from 'react-hook-form';
 
-import DropHereImage from 'assets/mock/production/DropHere.png';
 import Title from 'components/title';
+import useAxios from 'hooks/use-axios';
+
+function TextInput({ label, id, type, className, multiline }) {
+    const { register } = useFormContext();
+    const inputStyle = useMemo(
+        () => css`
+            width: 100%;
+            padding: 6px;
+            grid-column: 1;
+            grid-row: 2;
+            line-height: 1rem;
+            resize: none;
+            border-radius: 4px;
+        `,
+        [],
+    );
+    return (
+        <label
+            htmlFor={id}
+            className={className}
+            css={css`
+                display: grid;
+                grid-template-columns: auto;
+                grid-template-rows: auto 1fr;
+            `}
+        >
+            <p
+                css={css`
+                    margin: 0;
+                    font-size: 1.1rem;
+                    font-weight: bold;
+                    color: #ffffff;
+                    margin-bottom: 4px;
+                    grid-column: 1;
+                    grid-row: 1;
+                `}
+            >
+                {label}
+            </p>
+            {multiline ? (
+                <textarea id={id} css={inputStyle} {...register(id)} />
+            ) : (
+                <input id={id} type={type} css={inputStyle} {...register(id)} />
+            )}
+        </label>
+    );
+}
+
+TextInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    type: PropTypes.oneOf(['text', 'number']),
+    className: PropTypes.string,
+    multiline: PropTypes.bool,
+};
+
+TextInput.defaultProps = {
+    label: undefined,
+    type: 'text',
+    className: undefined,
+    multiline: false,
+};
 
 const Button = styled.button`
     background-color: rgba(114, 98, 168, 1);
     border: none;
     color: white;
-    padding: 1% 10%;
+    padding: 6px 12px;
     text-align: center;
     font-weight: bold;
     font-size: 20px;
-    margin: 1% 1% 1% 11%;
-    border-radius: 10px;
+    border-radius: 4px;
 `;
 
-export default function Achievements() {
-    const theme = useTheme();
+export default function BadgeProduction() {
+    const form = useForm();
+
+    const onDrop = useCallback(
+        (files) => {
+            if (Array.isArray(files) && files.length > 0) {
+                form.setValue('image', files[0]);
+            }
+        },
+        [form],
+    );
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+    });
+
+    const axios = useAxios();
+
+    const onSubmit = useCallback(
+        (values) => {
+            const data = new FormData();
+
+            Object.entries(values).forEach(([key, value]) => {
+                if (key === 'field') {
+                    data.append(key, value[1]);
+                } else {
+                    data.append(key, value);
+                }
+            });
+
+            axios.post('/badge', data);
+        },
+        [axios],
+    );
 
     return (
         <div
             // 전체 컨테이너 박스
             css={css`
                 width: 1000px;
-                height: 100%;
-                margin: auto;
+                margin: 0 auto;
             `}
         >
             <Title>뱃지 만들기</Title>
-            <div
-                // 큰 뱃지 생성 박스
-                css={css`
-                    background-color: rgba(62, 53, 92, 1);
-
-                    width: 900px;
-                    height: 480px;
-                    margin: 0% 5%;
-                    border-radius: 10px;
-
-                    display: grid;
-                `}
-            >
-                <div
-                    // 업적 사진 박스 (좌)
+            <FormProvider {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    // 큰 뱃지 생성 박스
                     css={css`
-                        background-color: rgba(239, 239, 239, 0.19);
-                        grid-row: 1;
-                        grid-column: 1;
-                        grid-column-end: 4;
+                        background-color: rgba(62, 53, 92, 1);
 
-                        width: 250px;
-                        height: 100px;
-                        padding: 20% 0;
-                        margin: 20% 20% 1% 20%;
+                        width: 100%;
+                        border-radius: 6px;
 
-                        text-align: center;
-                    `}
-                >
-                    <img
-                        src={DropHereImage}
-                        alt="Drop Here"
-                        css={css`
-                            max-width: 35%;
-                        `}
-                    />
-                </div>
-                <div
-                    // 업적 박스 (우)
-                    css={css`
-                        grid-row: 1;
-                        grid-column: 4;
+                        display: grid;
+                        grid-template-columns: 1fr 1fr 1fr 1fr;
+                        grid-template-rows: 1fr 0.2fr;
 
-                        width: 500px;
-                        height: auto;
-                        margin: 9% 0% 1% 0%;
+                        row-gap: 20px;
+                        column-gap: 20px;
+
+                        padding: 20px;
                     `}
                 >
                     <div
-                        // 업적 입력 박스
+                        type="button"
                         css={css`
-                            color: ${theme.colors.white};
-                            font-size: 18px;
-                            font-weight: bold;
-                            text-align: left;
+                            background-color: rgba(239, 239, 239, 0.19);
+                            grid-row: 1;
+                            grid-column: 1;
 
-                            width: auto;
-                            height: auto;
+                            aspect-ratio: 1;
+
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+
+                            color: #ffffff;
+
+                            border: 0;
+                            border-radius: 4px;
+                        `}
+                        {...getRootProps()}
+                    >
+                        <input
+                            {...getInputProps()}
+                            css={css`
+                                position: absolute;
+                                opacity: 0;
+                                pointer-events: none;
+                            `}
+                        />
+                        Drop Here
+                    </div>
+                    <div
+                        // 업적 박스 (우)
+                        css={css`
+                            grid-row: 1;
+                            grid-column-start: 2;
+                            grid-column-end: 5;
+                            width: 100%;
+
+                            display: grid;
+                            grid-template-columns: 1fr 1fr;
+                            grid-template-rows: auto 1fr;
+                            column-gap: 20px;
+                            row-gap: 20px;
                         `}
                     >
-                        <label>
-                            <p>
-                                업적명
-                                <input
-                                    css={css`
-                                        width: 70%;
-                                        height: 30%;
-                                        border-radius: 10px;
-                                        margin: 0 38px;
-                                    `}
-                                    type="text"
-                                    maxLength="15"
-                                />
-                            </p>
-                            <p>
-                                업적설명
-                                <input
-                                    css={css`
-                                        width: 70%;
-                                        height: auto;
-                                        padding: 0 0 12% 0;
-                                        border-radius: 10px;
-                                        margin: 0 20px;
-                                    `}
-                                    type="text"
-                                    maxLength="200"
-                                />
-                            </p>
-                            <p>
-                                획득조건
-                                <input
-                                    css={css`
-                                        width: 70%;
-                                        height: auto;
-                                        padding: 0 0 7% 0;
-                                        border-radius: 10px;
-                                        margin: 0 20px;
-                                    `}
-                                    type="text"
-                                    maxLength="200"
-                                />
-                            </p>
-                            <p>
-                                경험치
-                                <input
-                                    css={css`
-                                        width: 10%;
-                                        height: 10%;
-                                        border-radius: 10px;
-                                        margin: 0 38px;
-                                    `}
-                                    type="number"
-                                    min="0"
-                                    max="999"
-                                />
-                            </p>
-                        </label>
+                        <TextInput
+                            id="name"
+                            label="이름"
+                            css={css`
+                                grid-column: 1;
+                                grid-row: 1;
+                            `}
+                        />
+                        <TextInput
+                            id="exp"
+                            label="획득 경험치"
+                            type="number"
+                            css={css`
+                                grid-column: 2;
+                                grid-row: 1;
+                            `}
+                        />
+                        <TextInput
+                            id="condition"
+                            label="획득 조건"
+                            css={css`
+                                grid-column: 1;
+                                grid-row: 2;
+                            `}
+                            multiline
+                        />
+                        <TextInput
+                            id="desc"
+                            label="설명"
+                            css={css`
+                                grid-column: 2;
+                                grid-row: 2;
+                            `}
+                            multiline
+                        />
                     </div>
-                </div>
-                <div
-                    // 생성, 취소 버튼
-                    css={css`
-                        width: 100%;
-                        height: auto;
-
-                        grid-row: 2;
-                        grid-column-start: 2;
-                        grid-column-end: 5;
-                    `}
-                >
-                    <Button>생성</Button>
-                    <Button>취소</Button>
-                </div>
-            </div>
+                    <Button
+                        css={css`
+                            grid-column-start: 1;
+                            grid-column-end: 3;
+                            grid-row: 2;
+                        `}
+                    >
+                        생성
+                    </Button>
+                    <Button
+                        css={css`
+                            grid-column-start: 3;
+                            grid-column-end: 5;
+                            grid-row: 2;
+                        `}
+                    >
+                        취소
+                    </Button>
+                </form>
+            </FormProvider>
         </div>
     );
 }
